aba928f8bb818e4e69be3ee390a38aec39e5e55d
NoXF <xianyou.cyq@gmail.com>
2018-12-05 20:07:03 +0800

    remove unuse module declare

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-extern crate reqwest;
-extern crate base64;
-extern crate chrono;
-extern crate crypto;
 #[macro_use]
 extern crate failure;
 #[macro_use]
 extern crate log;
-extern crate xml;
-extern crate futures;
 
 pub mod oss;
 pub mod prelude;
